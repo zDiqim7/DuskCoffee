3d14859496cd51f450eab90c43915e03b2b5d4e4
zDiqim <quantum99q@gmail.com>
2026-06-03 17:36:53 +0700

    Add new Products section and change about the more information below about-us

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');  
 // ketika hamburger menu diklik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+// Js Carikan saya element yang nama id nya search-button, 
+document.querySelector('#search-button').onclick = (e) => { // ketika di click jalankan function (arrow function) berikut 
+  searchForm.classList.toggle('active'); // yaitu toggle class active untuk class="search form"
+  searchBox.focus();
+  e.preventDefault();
+}
+
+// klik di luar element
 const hamburgerMenu = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
 document.addEventListener('click', function(e){
   if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
     navbarNav.classList.remove('active');
+  }
+  if(!searchButton.contains(e.target) && !searchForm.contains(e.target)){
+    searchForm.classList.remove('active');
   }
 });
