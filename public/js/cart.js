e68fd28ed2ef7094f4a9c7d73f43f12598fc6d99
zdiqim7 <quantum99q@gmail.com>
2026-08-23 17:06:24 +0700

    removed unneeded files

--- public/js/cart.js
+++
@@ -1,7 +0,0 @@
-document.addEvemtListener('alpine:init', () => {
-  Alpine.data('cart', () => ({
-    items: {
-      { id: 1, name: 'Arabica Decaf', img: '1.html', price: 20000},
-    }
-  }));
-});
